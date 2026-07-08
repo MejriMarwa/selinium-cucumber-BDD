a951e95d84a3599094e961c7fd4b9f880f6f5df8
marwa <mejrimarwa40@gmail.com>
2026-07-08 21:21:27 +0100

    update project

---
+++ src/test/java/com/e2eTest/automation/page_objects/LoginPage.java
@@ -0,0 +1,13 @@
+package com.e2eTest.automation.page_objects;
+
+
+import com.e2eTest.automation.utils.BasePage;
+import com.e2eTest.automation.utils.Setup;
+
+public class LoginPage extends BasePage {
+	
+	 public LoginPage() {
+		super(Setup.getDriver());
+	 }
+
+}
